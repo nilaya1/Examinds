c097c97c29890c78d6757313caf7966c5b0e6d14
nilaya1 <nilaya.more15@gmail.com>
2022-09-17 00:29:24 +0530

    testboardsection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations" 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { TestboardComponent } from './testboard/testboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    TestboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/testboard/testboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testboard',
+  templateUrl: './testboard.component.html',
+  styleUrls: ['./testboard.component.scss']
+})
+export class TestboardComponent implements OnInit {
+
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+  }
+  valueIconLeft:any
+
+ 
+}
