a03c4c19456267fd5fa101269a531cf2831e1d2a
vaidehi <vaidehibansode20002@gmail.com>
2022-09-17 17:53:30 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
