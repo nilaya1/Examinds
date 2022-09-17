e913fc2cc1c093580ca7f356c8029570dbb03f3f
Ritz <ritkum93@gmail.com>
2022-09-17 17:40:58 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { DiscussComponent } from './discuss/discuss.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    DiscussComponent
   ],
   imports: [
     BrowserModule,
